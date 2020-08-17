21917e5d410ae27a9bacaea6d03e694d600509d7
David_Roark <duyvucrl@gmail.com>
2020-08-17 21:11:03 +0700

    Feature: Comment, Tagging => completed!

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ApplicationRecord
+  has_many :comments, dependent: :delete_all
+  has_many :taggings, dependent: :delete_all
+  has_many :tags, through: :taggings, dependent: :delete_all
+
+  def tag_list
+    tags.collect do |tag|
+      tag.name
+    end.join(', ')
+  end
+
+  def tag_list=(tags_string)
+    tags_name = tags_string.split(',').collect { |s| s.strip.downcase }.uniq
+    new_or_found_tag = tags_name.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tag
+  end
 end
